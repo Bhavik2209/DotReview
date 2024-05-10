8d1b8f707bb4dd9a98644cfd9c892f395a31df1b
Bhavik2209 <bhavikrohit22@gmail.com>
2024-05-11 00:20:52 +0530

    add files

---
+++ home/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render,HttpResponse
+
+def index(request):
+    return render(request,"index.html")
+
+def about(request):
+    return render(request,"about.html")
+
+def contact(request):
+    return render(request,"contact.html")
+
+def review(request):
+    return render(request,"review.html")
+
+
